7eeb74abff44a4e5b9ba29307b2311b980877109
James King <jcking1034@g.ucla.edu>
2022-02-17 19:27:19 +0000

    ELFDEV-355: Jking/filter completeness times

--- tst/output/metric/test_completeness.py
+++ tst/output/metric/test_completeness.py
@@ -1,3 +1,5 @@
+import datetime as dt
+
 import pandas as pd
 
 from data_type.completeness_config import COMPLETENESS_CONFIG_MAP
@@ -16,3 +18,38 @@ class TestCompletenessUpdater:
             completeness_updater.update_completeness_table_with_single_idpu_type(processing_request, empty_df, False)
             is False
         )
+
+    def test_split_science_zones(self):
+        pipeline_config = DummyPipelineConfig()
+        processing_request = DummyProcessingRequest()
+
+        # Only keep science zones that occur on the day of the processing_request
+        times = pd.Series(
+            [
+                dt.datetime.combine(processing_request.date, dt.datetime.min.time())
+                + dt.timedelta(days=-1, seconds=3 * i)
+                for i in range(5)  # Delete, data is all on the day before
+            ]
+            + [
+                dt.datetime.combine(processing_request.date, dt.datetime.min.time()) + dt.timedelta(seconds=3 * i)
+                for i in range(-3, 2)  # Keep since some data is on the current day
+            ]
+            + [
+                dt.datetime.combine(processing_request.date, dt.datetime.min.time())
+                + dt.timedelta(hours=12, seconds=3 * i)
+                for i in range(5)  # Keep since all data is on the current day
+            ]
+            + [
+                dt.datetime.combine(processing_request.date, dt.datetime.min.time())
+                + dt.timedelta(days=1, seconds=3 * i)
+                for i in range(5)  # Delete, data is all on the day after
+            ]
+        )
+
+        completeness_updater = CompletenessUpdater(pipeline_config.session, COMPLETENESS_CONFIG_MAP)
+        szs = completeness_updater.split_science_zones(processing_request, times)
+
+        assert len(szs) == 2
+        for sz in szs:
+            assert len(sz) == 5
+            assert any(t.date() == processing_request.date for t in sz)
