7803fa045f3577ad75abceffa5463751822516db
James King <jcking1034@g.ucla.edu>
2022-04-04 23:49:44 +0000

    Convert PacketInfo, ProcessingRequest to dataclasses; Make CompletenessConfig frozen

--- src/data_type/processing_request.py
+++ src/data_type/processing_request.py
@@ -1,51 +1,20 @@
 """Defines a class to describe a processing job that must be performed."""
 
 import datetime as dt
+from dataclasses import dataclass
 from typing import List
 
 from util import science_utils
 from util.constants import MISSION_DICT
 
 
+@dataclass(frozen=True, order=True)
 class ProcessingRequest:
     """A class to create objects describing files to generate"""
 
-    def __init__(self, mission_id: int, data_product: str, date: dt.date):
-        """Constructor for ProcessingRequest class.
-        mission_id = 1, 2, 3
-        data_product (SHOULD BE STRING)
-        date = date for which to generate file (ex. 2020-08-05)
-        """
-        self.mission_id = mission_id
-        self.data_product = data_product
-        self.date = date  # Should be a DATE, not a DATETIME
-
-    def __eq__(self, other) -> bool:
-        return (
-            self.mission_id == other.mission_id and self.data_product == other.data_product and self.date == other.date
-        )
-
-    def __lt__(self, other) -> bool:
-        return (self.mission_id, self.data_product, self.date) < (other.mission_id, other.data_product, other.date)
-
-    def __hash__(self) -> int:
-        return hash((self.mission_id, self.data_product, self.date))
-
-    def __str__(self) -> str:
-        return (
-            "ProcessingRequest("
-            + f"mission_id={self.mission_id}, "
-            + f"data_product={self.data_product}, "
-            + f"date={self.date})"
-        )
-
-    def __repr__(self) -> str:
-        return (
-            "ProcessingRequest("
-            + f"mission_id={self.mission_id}, "
-            + f"data_product={self.data_product}, "
-            + f"date={self.date})"
-        )
+    mission_id: int
+    data_product: str
+    date: dt.date
 
     @property
     def probe(self) -> str:
